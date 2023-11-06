30d6dcac393638278b3e8a00c96a0289c23dbdf1
Ye Tian <ye.tian@iqr-robot.com>
2023-11-06 15:19:49 +0800

    for humble

--- src/monocular/monocular-slam-node.cpp
+++ src/monocular/monocular-slam-node.cpp
@@ -10,7 +10,7 @@ MonocularSlamNode::MonocularSlamNode(ORB_SLAM3::System* pSLAM)
     m_SLAM = pSLAM;
     // std::cout << "slam changed" << std::endl;
     m_image_subscriber = this->create_subscription<ImageMsg>(
-        "camera",
+        "/camera/color/image_raw",
         10,
         std::bind(&MonocularSlamNode::GrabImage, this, std::placeholders::_1));
     std::cout << "slam changed" << std::endl;
